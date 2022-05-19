08b467095d5db9954d1988c7d12fb4084c5abe71
Philipp Riedel <riedel.philipp@gmx.net>
2022-05-19 15:36:09 +0200

    adds list controller methods incl. views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,9 +7,28 @@ class ListsController < ApplicationController
     @list = find_list
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    # No need for app/views/lists/create.html.erb
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def find_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
