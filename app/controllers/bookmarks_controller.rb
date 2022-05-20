2c00f100a5c4cb8c3bb82ed2cacf367aedce3b40
Philipp Riedel <riedel.philipp@gmx.net>
2022-05-20 09:25:58 +0200

    adds adjustments to route, controller and views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,32 @@
 class BookmarksController < ApplicationController
+  # We need to find the list associated with the bookmark
+  before_action :set_list, only: %i[new create]
+
+  def new
+    # We need @list in our `simple_form_for`
+    @list = set_list
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list_id = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+    raise
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
